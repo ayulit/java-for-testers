1ac3ea14667988c8237621e40563b3fae5d138aa
Andrey Litvinov <litwinoff@gmail.com>
2016-12-14 17:40:45 +0300

    Thread creating by anonymous class

--- src/main/java/lesson12/threads/Main.java
+++ src/main/java/lesson12/threads/Main.java
@@ -8,8 +8,17 @@ public class Main {
 		Thread guiThread = new Thread(new GuiThread());
 		Thread dataProcessingThread = new DataProcessingThread();
 		
+		// creating thread using anonymous class
+		Thread httpThread = new Thread(new Runnable(){
+											  public void run() {
+												  System.out.println("httpThread worked...");
+											  }
+										   }
+		                        );
+		
 		guiThread.start();
 		dataProcessingThread.start();
+		httpThread.start();
 		
 		System.out.println("mainThread worked...");
 	}
