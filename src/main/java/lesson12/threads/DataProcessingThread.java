c82b17e46c3c92d2eba75eb159c39e1239ec3877
Andrey Litvinov <litwinoff@gmail.com>
2016-12-14 18:26:02 +0300

    Thread sleep

--- src/main/java/lesson12/threads/DataProcessingThread.java
+++ src/main/java/lesson12/threads/DataProcessingThread.java
@@ -4,6 +4,18 @@ public class DataProcessingThread extends Thread {
 	
 	@Override
 	public void run() {
-		System.out.println("dataProcessingThread worked...");
+		
+		long time = 1000; // seconds
+		
+		System.out.println("dataProcessingThread started...");
+		
+		try {
+			Thread.sleep(time); // THIS thread will sleep
+		} catch (InterruptedException e) {
+			// occures if thread will be interrupted before time will have came 
+			System.out.println("dataProcessingThread: wake up!");
+		}
+		
+		System.out.println("dataProcessingThread: good morning!");
 	} 
 }
